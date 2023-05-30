f2ba203ad9465f9d05f385fbeee1d94fa119ca61
SydorenkoKateryna <sydorenko.k.ih@gmail.com>
2023-05-30 20:55:03 +0300

    custom hooks (useSortedPosts, usePosts)

--- src/pages/General.jsx
+++ src/pages/General.jsx
@@ -1,5 +1,6 @@
 import { Link, Outlet, useLocation } from 'react-router-dom';
-import { Suspense, useState, useMemo } from 'react';
+import { Suspense, useState } from 'react';
+import { usePosts } from 'components/hooks/usePosts';
 import BackLink from 'components/BackLink';
 import Counter from 'components/Counter';
 import ClassCounter from 'components/ClassCounter';
@@ -7,37 +8,18 @@ import PostList from 'components/PostList';
 import PostForm from 'components/PostForm';
 import PostFilter from 'components/PostFilter';
 import MyModal from 'components/UI/modal/MyModal';
-// import MySelect from 'components/UI/select/MySelect';
-// import MyInput from 'components/UI/input/MyInput';
 import MyButton from 'components/UI/button/MyButton';
 
 const General = () => {
   const location = useLocation();
   const backLinkHref = location.state?.from ?? '/practice';
 
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'JS 1', body: 'Description' },
-    { id: 2, title: 'JS 2', body: 'Description' },
-    { id: 3, title: 'JS 3', body: 'Description' },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [isModalVisble, setIsModalVisible] = useState(false);
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [posts, filter.sort]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = newPost => {
     setPosts(prev => [...prev, newPost]);
@@ -69,13 +51,10 @@ const General = () => {
 
       <div>
         <h2>Form</h2>
-
         <MyButton onClick={() => setIsModalVisible(true)}>
           Create a post
         </MyButton>
-
         <hr style={{ margin: '15px 0' }} />
-
         <PostFilter filter={filter} setFilter={setFilter} />
       </div>
 
